f2fefc34351c05fa3b9149dab77dbb33b21fdb59
Shruti gangal <shrutigangal6295@gmail.com>
2021-02-11 14:33:57 +0100

    To trigger jenkins just given space between lines

--- com.nozama.test/src/test/java/com/nozama/test/Adminsupprclient.java
+++ com.nozama.test/src/test/java/com/nozama/test/Adminsupprclient.java
@@ -61,6 +61,7 @@ public class Adminsupprclient {
   
   @Test
   public void adminsupprclient() {
+	  
     driver.findElement(By.id("edit-name")).click();
     driver.findElement(By.id("edit-name")).sendKeys("admin1");
     driver.findElement(By.id("edit-pass")).click();
