4d76986ffcc98a87a87c24fac50fc6bd1658e63e
Shruti gangal <shrutigangal6295@gmail.com>
2021-02-13 20:28:34 +0100

    space in the AllTests.java

--- com.nozama.test/src/test/java/com/nozama/test/AllTests.java
+++ com.nozama.test/src/test/java/com/nozama/test/AllTests.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
+
 @SuiteClasses({ Testcreationclient.class, Produitdanspanier.class, Adminsupprclient.class })
 public class AllTests {
 
